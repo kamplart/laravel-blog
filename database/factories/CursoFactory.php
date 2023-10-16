bc2fc1e066e2f9d7a4aabdb742a3d7b4d6c4dd7a
CamiloAir <dev1@e-advising.biz>
2023-10-16 18:03:47 -0500

    Feed Factory

---
+++ database/factories/CursoFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Curso>
+ */
+class CursoFactory extends Factory
+{
+
+    //protected $model = Curso::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name'=>$this->faker->sentence,
+            'description'=>$this->faker->paragraph,
+            'categoria'=>$this->faker->randomElement(['Desarrollo web', 'Diseño web'])
+            //
+        ];
+    }
+}
--- database/seeders/CursoSeed.php
+++ database/seeders/CursoSeed.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Curso;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,40 +14,7 @@ class CursoSeed extends Seeder
      */
     public function run(): void
     {
-
-        /*
-        $data[] = array('name'=>'Laravel','description' => 'El mejor FrameWork', 'categoria'=>'web' );
-        $data[] = array('name'=>'Laravel','description' => 'El mejor FrameWork', 'categoria'=>'web' );
-        $data[] = array('name'=>'Laravel','description' => 'El mejor FrameWork', 'categoria'=>'web' );
-        $data[] = array('name'=>'Laravel','description' => 'El mejor FrameWork', 'categoria'=>'web' );
-
-        foreach($data as $val){
-
-            $curso = new Curso();
-            $curso->name = $val['Laravel'];
-            $curso->description = $val['description'];
-            $curso->categoria = $val['categoria'];
-            $curso->save();
-        }*/
-
-        $curso = new Curso();
-        $curso->name = 'Laravel';
-        $curso->description = 'El mejor FrameWork';
-        $curso->categoria = 'Web';
-        $curso->save();
-
-        $curso = new Curso();
-        $curso->name = 'Laravel';
-        $curso->description = 'El mejor FrameWork';
-        $curso->categoria = 'Web';
-        $curso->save();
-
-
-        $curso = new Curso();
-        $curso->name = 'Laravel';
-        $curso->description = 'El mejor FrameWork';
-        $curso->categoria = 'Web';
-        $curso->save();
-
+        Curso::factory(50)->create();
+        User::factory(10)->create();
     }
 }
